9b2f9eebff52445eb2d38a653f0c0210d4ed120c
vinicius-grandi <viniciusggrandi@gmail.com>
2022-11-21 23:17:21 -0300

    create some common elements

--- app/src/main/java/com/vinicius_grandi/androidbasics/MainActivity.java
+++ app/src/main/java/com/vinicius_grandi/androidbasics/MainActivity.java
@@ -3,6 +3,7 @@ package com.vinicius_grandi.androidbasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
